fa20d74d7d607a5aeb8b646c7bca4823e07ba631
AndriiKozlovskyi <andrii@software.com>
2025-02-20 01:20:38 +0100

    once more

--- components/Slide.vue
+++ components/Slide.vue
@@ -1,5 +1,5 @@
 <template>
-     <div class="w-screen sm:h-[45rem] h-[calc(100vh-15vh)] flex flex-col bg-cover bg-center rounded-xl opacity-90"
+     <div class="w-screen sm:h-[45rem] h-[calc(100vh-12vh)] flex flex-col bg-cover bg-center rounded-xl opacity-90"
         :style="{ backgroundImage: `url(${image})` }"
     >
         <div class="opacity-90 bg-black inset-0 flex flex-col justify-between z-20 bg-cover w-full h-full">
