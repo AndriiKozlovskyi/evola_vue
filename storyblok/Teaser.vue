3902305a0b4ecde54f103a0b8297cf3b74ad740a
AndriiKozlovskyi <andrii@software.com>
2025-02-22 16:57:54 +0100

    fixed teaser

--- storyblok/Teaser.vue
+++ storyblok/Teaser.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-editable="blok" class="px-4">
         <div class="py-16 text-5xl font-bold text-center">
-            <h1 class="text-xl">{{ block.name }}</h1>
+            <!-- <h1 class="text-xl">{{ block.name }}</h1> -->
         </div>
     </div>
   </template>
